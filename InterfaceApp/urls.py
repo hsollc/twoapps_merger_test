12598bac606c21e82fcba20940105897d9dd3c11
SUBIN_LAPTOP\ooeee_000 <ooeeee@naver.com>
2022-10-05 23:47:37 +0900

    index 페이지 내용 수정 등
    - 템플릿 내용 수정
    - base.html 생성
    - news.html 생성 및 urls 업데이트

--- InterfaceApp/urls.py
+++ InterfaceApp/urls.py
@@ -1,8 +1,9 @@
-from django.urls import  path, include
+from django.urls import path, include
 from . import views
 
 app_name = 'InterfaceApp'
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('news/', views.news, name='news'),
 ]
