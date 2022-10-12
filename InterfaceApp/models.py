a6b88362dfb67a6d6ab2287849f78d73661153f0
SUBIN_LAPTOP\ooeee_000 <ooeeee@naver.com>
2022-10-12 17:16:42 +0900

    DB 모델 생성
    -  공연/전시 정보 데이터용 테이블

--- InterfaceApp/models.py
+++ InterfaceApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class PerformanceList(models.Model):
+    seq = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200)
+    startDate = models.IntegerField()
+    endDate = models.IntegerField()
+    place = models.CharField(max_length=50)
+    realmName = models.CharField(max_length=10)
+    area = models.CharField(max_length=10, null=True)
+    thumbnail = models.CharField(max_length=256)
+    gpsX = models.DecimalField(null=True)
+    gpsY = models.DecimalField(null=True)
+
+    def __str__(self):
+        return f"seq: {self.seq}, title: {self.title}, startDate: {self.startDate}, endDate: {self.endDate}, place: {self.place}, realmName: {self.realmName}, area: {self.area}, thumbnail: {self.thumbnail}, gpsX: {self.gpsX}, gpsY: {self.gpsY}"
+
