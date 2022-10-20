5e1fe527bffdf4c55632bac42bffc25ba8252afc
SUBIN_LAPTOP\ooeee_000 <ooeeee@naver.com>
2022-10-20 13:00:41 +0900

    문화포털 오픈API 서비스키 공유

--- InterfaceApp/api.py
+++ InterfaceApp/api.py
@@ -1,10 +1,21 @@
 from urllib.parse import urlencode, unquote, quote_plus
 import requests
 from bs4 import BeautifulSoup
+from xml.etree.ElementTree import parse
+import xmltodict
 
 # 문화포털 공연전시정보 조회서비스 오픈API 서비스 키
-serviceKey = "n1Jh450%2FmskemPK5lqiA2uBPBeE3ypioAAuWbZ6nNHIsTCnAi6Q7R%2F3IDXnvrx3lGcEyzkHXRM5CqpUyxLMNhA%3D%3D"
-serviceKeyDecoded = unquote(serviceKey, 'UTF-8')
+service_key = "n1Jh450%2FmskemPK5lqiA2uBPBeE3ypioAAuWbZ6nNHIsTCnAi6Q7R%2F3IDXnvrx3lGcEyzkHXRM5CqpUyxLMNhA%3D%3D"
+serviceKeyDecoded = unquote(service_key, 'UTF-8')
 
-def performance_search():
+def performance_api():
     url = "http://www.culture.go.kr/openapi/rest/publicperformancedisplays/period"
+    params = {'from': '20220101',
+              'to': '20231231',
+              'cPage': '1',
+              'rows': '10',
+              'serviceKey': service_key
+              }
+
+
+
